899b6cd9a51f8bdffd7ebcb9bb586c22864ba5f3
samualkillerz <samualkillerz@gmail.com>
2026-05-10 16:11:33 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -2,43 +2,45 @@ from database.mongo import users_db
 
 
 async def is_user_exist(user_id):
+
     user = await users_db.find_one(
         {"user_id": user_id}
     )
+
     return bool(user)
 
 
 async def add_user(user_id, name):
+
     data = {
         "user_id": user_id,
         "name": name,
         "credits": 5,
-        "joined": False,
-        "unlocked": False,
         "referrals": 0,
-        "daily_searches": 3,
-        "premium": False,
-        "banned": False
+        "unlocked": False
     }
 
     await users_db.insert_one(data)
 
 
 async def get_user(user_id):
+
     return await users_db.find_one(
         {"user_id": user_id}
     )
 
 
-async def add_credits(user_id, amount):
+async def add_referral(user_id):
+
     await users_db.update_one(
         {"user_id": user_id},
-        {"$inc": {"credits": amount}}
+        {"$inc": {"referrals": 1}}
     )
 
 
-async def remove_credits(user_id, amount):
+async def unlock_user(user_id):
+
     await users_db.update_one(
         {"user_id": user_id},
-        {"$inc": {"credits": -amount}}
+        {"$set": {"unlocked": True}}
     )
