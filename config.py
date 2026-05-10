a7626892218f98cefe035da2cf94c27d5dcd2f34
samualkillerz <samualkillerz@gmail.com>
2026-05-10 11:18:27 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+MONGO_URI = getenv("MONGO_URI")
+
+ADMINS = list(map(int, getenv("ADMINS").split()))
+
+STORAGE_CHANNEL = int(getenv("STORAGE_CHANNEL"))
+LOG_CHANNEL = int(getenv("LOG_CHANNEL"))
+
+FORCE_SUB_CHANNELS = list(
+    map(int, getenv("FORCE_SUB_CHANNELS").split())
+)
+
+BOT_USERNAME = getenv("BOT_USERNAME")
