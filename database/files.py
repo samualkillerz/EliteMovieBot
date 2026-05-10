0da8478273bc1a1065a26914fe298a31d1e1ea45
samualkillerz <samualkillerz@gmail.com>
2026-05-10 20:43:14 +0530

    Update files.py

--- database/files.py
+++ database/files.py
@@ -11,3 +11,18 @@ async def get_file_by_unique(unique_id):
 async def add_file(data):
 
     await files_db.insert_one(data)
+
+
+
+async def search_files(query):
+
+    files = files_db.find(
+        {
+            "file_name": {
+                "$regex": query,
+                "$options": "i"
+            }
+        }
+    ).limit(10)
+
+    return await files.to_list(length=10)
