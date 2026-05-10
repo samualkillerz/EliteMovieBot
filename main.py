7bc67c712278019fcd3c5eaa5d4340ed417e48d7
samualkillerz <samualkillerz@gmail.com>
2026-05-10 12:31:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,11 @@ from pyrogram import Client, idle
 from config import *
 
 app = Client(
-    "ZlixBot",
+    "LordVT4",
     api_id=API_ID,
     api_hash=API_HASH,
     bot_token=BOT_TOKEN,
     plugins=dict(root="plugins")
 )
 
-app.start()
-
-me = app.get_me()
-
-print(f"Bot Started : @{me.username}")
-
-idle()
-
-app.stop()
+app.run()
