92e470fc43f2d6111d9009eacb0458fe6fd95f45
samualkillerz <samualkillerz@gmail.com>
2026-05-10 13:03:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,6 +6,8 @@ from database.users import (
     add_user
 )
 
+from database.mongo import files_db
+
 
 @Client.on_message(filters.private & filters.command("start"))
 async def start_command(client, message: Message):
@@ -16,10 +18,36 @@ async def start_command(client, message: Message):
     if not await is_user_exist(user_id):
         await add_user(user_id, name)
 
-    await message.reply_text(
-        f"""
-Hello {name} 👋
+    # START PAYLOAD
+    data = message.command
 
-Welcome to LordVT4ProBot
-"""
+    if len(data) > 1:
+
+        payload = data[1]
+
+        # FILE LINK
+        if payload.startswith("file_"):
+
+            deep_link = payload.replace(
+                "file_", ""
+            )
+
+            file_data = await files_db.find_one(
+                {"deep_link": deep_link}
+            )
+
+            if not file_data:
+
+                return await message.reply_text(
+                    "File not found."
+                )
+
+            await message.reply_cached_media(
+                file_data["file_id"]
+            )
+
+            return
+
+    await message.reply_text(
+        f"Hello {name} 👋"
     )
