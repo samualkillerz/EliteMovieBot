345deb8b59e73d670b8d10186def95866060ed17
samualkillerz <samualkillerz@gmail.com>
2026-05-10 12:19:12 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,27 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from main import app
+
+from database.users import (
+    is_user_exist,
+    add_user
+)
+
+
+@app.on_message(filters.private & filters.command("start"))
+async def start_command(client, message: Message):
+
+    user_id = message.from_user.id
+    name = message.from_user.first_name
+
+    if not await is_user_exist(user_id):
+        await add_user(user_id, name)
+
+    await message.reply_text(
+        f\"\"\"
+Hello {name}
+
+Welcome to Veil Movies Bot.
+        \"\"\"
+    )
