2ba9efbbd7459af16a81d7fd77784b35f79b9cc2
samualkillerz <samualkillerz@gmail.com>
2026-05-11 14:00:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 MONGO_URI = getenv("MONGO_URI")
-TMDB_API = getenv(TMDB_API)
+TMDB_API = getenv("TMDB_API")
 
 ADMINS = list(map(int, getenv("ADMINS").split()))
 
