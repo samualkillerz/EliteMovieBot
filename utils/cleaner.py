0bfc4801887793f70d10a3361b41510e203dca68
samualkillerz <samualkillerz@gmail.com>
2026-05-11 18:41:56 +0530

    Create cleaner.py

---
+++ utils/cleaner.py
@@ -0,0 +1,78 @@
+import re
+
+
+BRANDING = "@LordVT4Pro"
+
+
+def clean_file_name(name):
+
+    # REMOVE EXTENSION
+    extension = ""
+
+    if "." in name:
+
+        extension = "." + name.split(".")[-1]
+
+        name = name.rsplit(".", 1)[0]
+
+    # REMOVE TELEGRAM TAGS
+    patterns = [
+
+        r"@\w+",
+
+        r"\[.*?\]",
+
+        r"\(.*?ESub.*?\)",
+
+        r"ESub",
+
+        r"www\.\S+",
+
+        r"MoviesMod",
+
+        r"mkvCinema",
+
+        r"Vegamovies",
+
+        r"HDHub4u",
+
+        r"Telly",
+
+        r"TamilBlasters",
+
+        r"Telugu",
+
+        r"Hindi Dubbed",
+
+        r"Official",
+
+        r"RymOfficial"
+    ]
+
+    for pattern in patterns:
+
+        name = re.sub(
+            pattern,
+            "",
+            name,
+            flags=re.IGNORECASE
+        )
+
+    # REMOVE EXTRA SYMBOLS
+    name = re.sub(
+        r"[_\-\.]+",
+        " ",
+        name
+    )
+
+    # REMOVE EXTRA SPACES
+    name = " ".join(
+        name.split()
+    )
+
+    # ADD YOUR BRANDING
+    cleaned = (
+        f"{name} {BRANDING}"
+    ).strip()
+
+    return cleaned + extension
