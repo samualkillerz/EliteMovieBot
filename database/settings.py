2d1b50bec56246a77f22b18e58e041ed4c594222
samualkillerz <samualkillerz@gmail.com>
2026-05-13 14:57:31 +0530

    Update settings.py

--- database/settings.py
+++ database/settings.py
@@ -1,26 +1,46 @@
 from database.mongo import settings_db
 
 
-async def save_thumb(file_id):
+DEFAULT_SETTINGS = {
+    "_id": "main",
+
+    "credits_enabled": False,
+
+    "auto_delete": False,
+
+    "delete_mode": "free",
+
+    "delete_timer": 120
+}
+
+
+async def get_settings():
+
+    settings = await settings_db.find_one(
+        {"_id": "main"}
+    )
+
+    if not settings:
+
+        await settings_db.insert_one(
+            DEFAULT_SETTINGS
+        )
+
+        settings = DEFAULT_SETTINGS
+
+    return settings
+
+
+async def update_setting(
+    key,
+    value
+):
 
     await settings_db.update_one(
-        {"_id": "thumb"},
+        {"_id": "main"},
         {
             "$set": {
-                "file_id": file_id
+                key: value
             }
-        },
-        upsert=True
+        }
     )
-
-
-async def get_thumb():
-
-    data = await settings_db.find_one(
-        {"_id": "thumb"}
-    )
-
-    if not data:
-        return None
-
-    return data.get("file_id")
